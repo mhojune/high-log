c535a8854028126626a313405edee8d25bde6898
javadocq <han18264@naver.com>
2026-02-03 22:53:37 +0900

    feat: add icon & svgr setting

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
 import * as S from '@/pages/Home/Home.styles';
+import User from '@/assets/icons/circle_user.svg?react';
 
 export default function Home() {
   return (
     <S.Container>
       <h1>Home</h1>
+      <User width={40} height={40} fill="red" />
     </S.Container>
   );
 };
