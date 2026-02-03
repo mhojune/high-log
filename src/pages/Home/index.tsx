8a5dfca8ba52afda652f73cc6f8a95b025a4ffb1
javadocq <han18264@naver.com>
2026-02-03 22:13:13 +0900

    feat: design color

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,15 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: ${props => props.theme.colors.labelColor['00']};
+`;
+
 export default function Home() {
   return (
-    <div>
+    <Container>
       <h1>Home</h1>
-    </div>
+    </Container>
   );
 };
