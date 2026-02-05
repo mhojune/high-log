d227fb7b3d515243351fdb7e4c1e8b5b40bc20d0
javadocq <han18264@naver.com>
2026-02-05 20:49:12 +0900

    feat: acodian

---
+++ src/components/acodian/Acodian.styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const AcodianIconContainer = styled.div<{type:string}>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 48px;
+  height: 48px;
+  cursor: pointer;
+
+  transition: transform 0.4s ease-in-out;
+  transform: ${({ type }) => (type === "default" ? 'rotate(0deg)' : 'rotate(180deg)')};
+`;
