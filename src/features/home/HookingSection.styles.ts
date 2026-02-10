7b12c0a599ff4acc3113d57025d443e081115e0e
javadocq <han18264@naver.com>
2026-02-11 04:39:19 +0900

    feat: hookingSection Card

--- src/features/home/HookingSection.styles.ts
+++ src/features/home/HookingSection.styles.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import Hooking_img from "@/assets/images/hooking.png";
+import { Link } from "react-router-dom";
 
 export const HookingSectionContainer = styled.div`
     width: 1440px;
@@ -53,4 +54,85 @@ export const HookingTextSub = styled.p`
 export const HookingTextSubBold = styled.span`
     ${({theme}) => theme.typography.body.L1};
     color: ${({theme}) => theme.colors.secondary["08"]};
+`;
+
+export const HookingCardWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 39px;
+`;
+
+export const HookingCardStatBox = styled.div`
+    display: flex;
+    width: 306px;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 8px;
+    border-radius: 16px;
+    background-color: #F8F8FA;
+`;
+
+export const HookingCardStat = styled.div`
+    height: 99px;
+    align-self: stretch;
+    border-radius: 16px;
+    background: linear-gradient(169deg, #CDD5F4 17.2%, #FAFAFA 93.94%);
+    position: relative;
+    overflow: hidden;
+`;
+
+export const HookingCardStatTextBox = styled.div`
+    position: absolute;
+    left: 24px;
+    top: 21.5px;
+    display: flex;
+    flex-direction: column;
+`;
+
+export const HookingCardStatTextMain = styled.p`
+    ${({theme}) => theme.typography.body.XS2};
+    color: ${({theme}) => theme.colors.secondary["00"]};
+`;
+
+export const HookingCardStatTextSub = styled.p`
+    ${({theme}) => theme.typography.head.H4};
+    color: ${({theme}) => theme.colors.grayScale["00"]};
+`;
+
+export const HookingCardStatImg = styled.img`
+    width: 127px;
+    height: 127px;
+    position: absolute;
+    left: 154px;
+    top: -30px;
+`;
+
+export const HookingEventCard = styled(Link)`
+    display: flex;
+    width: 306px;
+    height: 105px;
+    border-radius: 12px;
+    background-color: ${({theme}) => theme.colors.primary["00"]};
+    padding-left: 19px;
+    align-items: center;
+    overflow: hidden;
+    cursor: pointer;
+    text-decoration: none;
+`;
+
+export const HookingEventText = styled.p`
+    ${({theme}) => theme.typography.body.M2};
+    color: ${({theme}) => theme.colors.grayScale["11"]};
+`;
+
+export const HookingEventTextSub = styled.span`
+    ${({theme}) => theme.typography.body.M1};
+    color: ${({theme}) => theme.colors.grayScale["11"]};
+    white-space: pre-wrap;
+`;
+
+export const HookingEventImg = styled.img`
+    width: 125px;
+    height: 90px;
+    margin-top: 15px;
 `;
