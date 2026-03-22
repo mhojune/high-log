785addc2cf479109b8bd4cfff4b224d1abd62115
javadocq <han18264@naver.com>
2026-03-22 23:08:01 +0900

    feat: interview_practice storage page

--- src/components/filter/ListPopUp.styles.ts
+++ src/components/filter/ListPopUp.styles.ts
@@ -5,71 +5,74 @@ const spin = keyframes`
   100% { transform: rotate(360deg); }
 `;
 
-export const ListPopUpContainer = styled.div`
-    position: absolute;
-    top: calc(100% + 8px);
-    left: 50%;
-    transform: translateX(-50%);
-    z-index: 10;
-    display: flex;
-    width: 378px;
-    padding: 8px 16px;
-    flex-direction: column;
-    align-items: flex-start;
-    gap: 4px;
-    border-radius: 12px;
-    border: 0.5px solid ${({theme}) => theme.colors.grayScale["08"]};
-    background-color: ${({theme}) => theme.colors.grayScale["09"]};
-    max-height: 198px;
-    overflow-y: auto;
+export const ListPopUpContainer = styled.div<{
+  $width?: number;
+  $maxHeight?: number;
+}>`
+  position: absolute;
+  top: calc(100% + 8px);
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 10;
+  display: flex;
+  width: ${({ $width = 378 }) => `${$width}px`};
+  padding: 8px 16px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 4px;
+  border-radius: 12px;
+  border: 0.5px solid ${({ theme }) => theme.colors.grayScale["08"]};
+  background-color: ${({ theme }) => theme.colors.grayScale["09"]};
+  max-height: ${({ $maxHeight = 198 }) => `${$maxHeight}px`};
+  overflow-y: auto;
 
-    &::-webkit-scrollbar {
-        display: none;
-    }
-    
-    -ms-overflow-style: none;
-    scrollbar-width: none;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+
+  -ms-overflow-style: none;
+  scrollbar-width: none;
 `;
 
 export const LoaderWrapper = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    padding: 10px 0;
-    
-    & > svg {
-        animation: ${spin} 1s linear infinite;
-    }
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 10px 0;
+
+  & > svg {
+    animation: ${spin} 1s linear infinite;
+  }
 `;
 
 export const TitleWrapper = styled.div`
-    display: flex;
-    padding: 4px 2px;
-    flex-direction: column;
-    align-items: flex-start;
-    align-self: stretch;
-    cursor: pointer;
-    border-radius: 4px;
+  display: flex;
+  padding: 4px 2px;
+  flex-direction: column;
+  align-items: flex-start;
+  align-self: stretch;
+  cursor: pointer;
+  border-radius: 4px;
 
-    &:hover {
-        background-color: ${({theme}) => theme.colors.grayScale["08"]};
-    }
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.grayScale["08"]};
+  }
 `;
 
 export const Title = styled.p`
-    ${({theme}) => theme.typography.body.M0};
-    color: ${({theme}) => theme.colors.grayScale["04"]};
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
+  ${({ theme }) => theme.typography.body.M0};
+  color: ${({ theme }) => theme.colors.grayScale["04"]};
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
 
 export const Line = styled.div`
-    width: 100%;
-    height: 1px;
-    min-height: 1px;
-    flex-shrink: 0;
-    background-color: ${({theme}) => theme.colors.grayScale["06"]};
-    align-self: stretch;
+  width: 100%;
+  height: 1px;
+  min-height: 1px;
+  flex-shrink: 0;
+  background-color: ${({ theme }) => theme.colors.grayScale["06"]};
+  align-self: stretch;
 `;
--- src/components/filter/ListPopUp.tsx
+++ src/components/filter/ListPopUp.tsx
@@ -1,33 +1,41 @@
-import * as S from "@/components/filter/ListPopUp.styles"
-import LoaderIcon from "@/assets/icons/loader_circle.svg?react"
+import * as S from "@/components/filter/ListPopUp.styles";
+import LoaderIcon from "@/assets/icons/loader_circle.svg?react";
 import React from "react";
 
 interface ListPopUpProps {
-    data: {
-        fileId: number;
-        fileName: string;
-    }[];
-    onSelect: (fileName: string) => void;
-    isLoading?: boolean;
+  data: {
+    fileId: number;
+    fileName: string;
+  }[];
+  onSelect: (fileName: string) => void;
+  isLoading?: boolean;
+  width?: number;
+  maxHeight?: number;
 }
 
-export default function ListPopUp({ data, onSelect, isLoading }: ListPopUpProps) {
-    return (
-        <S.ListPopUpContainer>
-            {isLoading ? (
-                <S.LoaderWrapper>
-                    <LoaderIcon width={48} height={48} stroke="#737373" />
-                </S.LoaderWrapper>
-            ) : (
-                data.map((item, index) => (
-                    <React.Fragment key={item.fileId}>
-                        <S.TitleWrapper onClick={() => onSelect(item.fileName)}>
-                            <S.Title>{item.fileName}</S.Title>
-                        </S.TitleWrapper>
-                        {index < data.length - 1 && <S.Line />}
-                    </React.Fragment>
-                ))
-            )}
-        </S.ListPopUpContainer>
-    )
+export default function ListPopUp({
+  data,
+  onSelect,
+  isLoading,
+  width,
+  maxHeight,
+}: ListPopUpProps) {
+  return (
+    <S.ListPopUpContainer $width={width} $maxHeight={maxHeight}>
+      {isLoading ? (
+        <S.LoaderWrapper>
+          <LoaderIcon width={48} height={48} stroke="#737373" />
+        </S.LoaderWrapper>
+      ) : (
+        data.map((item, index) => (
+          <React.Fragment key={item.fileId}>
+            <S.TitleWrapper onClick={() => onSelect(item.fileName)}>
+              <S.Title>{item.fileName}</S.Title>
+            </S.TitleWrapper>
+            {index < data.length - 1 && <S.Line />}
+          </React.Fragment>
+        ))
+      )}
+    </S.ListPopUpContainer>
+  );
 }
