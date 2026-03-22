c9986147ff4e0b8580db7f4909dff3251ce56a24
Lee, Jong Won <138106799+javadocq@users.noreply.github.com>
2026-03-22 17:42:29 +0900

    feat: FAQ Page (#48)
    * fix: header sequence and button
    
    * feat: home page style
    
    * feat: faq page

--- src/constants/header/NAV_LIST.ts
+++ src/constants/header/NAV_LIST.ts
@@ -1,39 +1,41 @@
 export const NAV_LIST = [
-    {
-        id: 1,
-        title: "이용안내",
-        path: "/guide",
-    },
-    {
-        id: 2,
-        title: "면접 질문",
-        path: "/question",
-        sub_list: [
-            { title: "질문 생성", path: "/question" }, 
-            { title: "질문 보관함", path: "/question/storage" }
-        ]
-    },
-    {
-        id: 3,
-        title: "면접 연습",
-        path: "/interview/practice",
-        sub_list: [
-            { title: "면접 연습", path: "/interview/practice" }, 
-            { title: "면접 보관함", path: "/interview/practice/storage" }
-        ]
-    },
-    {
-        id: 4,
-        title: "생기부 관리",
-        path: "/record_management",
-    },
-    {
-        id: 5,
-        title: "고객센터",
-        path: "/support",
-        sub_list: [
-            { title: "공지사항", path: "/support" }, 
-            { title: "FAQ", path: "/support/faq" }
-        ]
-    },
-]
+  {
+    id: 1,
+    title: "생기부 관리",
+    path: "/record_management",
+  },
+
+  {
+    id: 2,
+    title: "면접 질문",
+    path: "/question",
+    sub_list: [
+      { title: "질문 생성", path: "/question" },
+      { title: "질문 보관함", path: "/question/storage" },
+    ],
+  },
+
+  {
+    id: 3,
+    title: "면접 연습",
+    path: "/interview/practice",
+    sub_list: [
+      { title: "면접 연습", path: "/interview/practice" },
+      { title: "면접 보관함", path: "/interview/practice/storage" },
+    ],
+  },
+  {
+    id: 4,
+    title: "이용안내",
+    path: "/guide",
+  },
+  {
+    id: 5,
+    title: "고객센터",
+    path: "/support",
+    sub_list: [
+      { title: "공지사항", path: "/support" },
+      { title: "FAQ", path: "/faq" },
+    ],
+  },
+];
