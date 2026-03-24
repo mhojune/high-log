7879dc91578b0996f602bae36a8b98de58cc50c5
June <91944313+mhojune@users.noreply.github.com>
2026-03-24 19:59:48 +0900

    feat: change authdescription and mainpage link

--- src/features/auth/AuthDescription.tsx
+++ src/features/auth/AuthDescription.tsx
@@ -1,6 +1,15 @@
 import { DefaultButton } from "@/components/button/Button";
 import { AUTH_DESCRIPTION, AUTH_FEATURE_BLOCKS } from "@/constants/auth";
 import * as S from "@/features/auth/AuthDescription.styles";
+import FileQuestionIcon from "@/assets/icons/file-question-01.svg?react";
+import MessageChatIcon from "@/assets/icons/message-chat-01.svg?react";
+import Book04Icon from "@/assets/icons/book-04.svg?react";
+
+const AUTH_FEATURE_ICONS = [
+  FileQuestionIcon,
+  MessageChatIcon,
+  Book04Icon,
+] as const;
 
 export default function AuthDescription() {
   return (
@@ -11,16 +20,19 @@ export default function AuthDescription() {
         ))}
       </S.TextBlock>
       <S.FeatureBlocksWrapper>
-        {AUTH_FEATURE_BLOCKS.map((block) => (
-          <S.FeatureBlock key={block.title}>
-            <S.FeatureBlockContent>
-              <S.FeatureBlockTitle>{block.title}</S.FeatureBlockTitle>
-              <S.FeatureBlockDescription>
-                {block.description}
-              </S.FeatureBlockDescription>
-            </S.FeatureBlockContent>
-          </S.FeatureBlock>
-        ))}
+        {AUTH_FEATURE_BLOCKS.map((block, index) => {
+          const Icon = AUTH_FEATURE_ICONS[index];
+          return (
+            <S.FeatureBlock key={block.title}>
+              <S.FeatureBlockContent>
+                <S.FeatureBlockIcon>
+                  <Icon aria-hidden />
+                </S.FeatureBlockIcon>
+                <S.FeatureBlockTitle>{block.title}</S.FeatureBlockTitle>
+              </S.FeatureBlockContent>
+            </S.FeatureBlock>
+          );
+        })}
       </S.FeatureBlocksWrapper>
       <S.ButtonWrapper>
         <DefaultButton
